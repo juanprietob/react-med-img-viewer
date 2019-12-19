8d964e0ccf1d14a7a6d2d2552107fd6cba0fdb8f
juanprietob <juanprietob@gmail.com>
2019-12-19 11:35:14 -0500

    ENH: Read the image using itk.js and vtk.js

--- src/med-img-reader.js
+++ src/med-img-reader.js
@@ -2,6 +2,7 @@
 import _ from 'underscore';
 import vtkImageData from 'vtk.js/Sources/Common/DataModel/ImageData';
 import vtkDataArray from 'vtk.js/Sources/Common/Core/DataArray';
+import ITKHelper from 'vtk.js/Sources/Common/DataModel/ITKHelper'
 
 const ISELECTRON = window && window.require && window.require('electron');
 const fs = ISELECTRON? window.require('electron').remote.require('fs'): undefined;
@@ -41,32 +42,7 @@ class MedImgReader {
   }
 
   convertToVtkImage(itkImage) {
-
-    if(itkImage){
-      const vtkImage = {
-        origin: [...itkImage.origin],
-        spacing: [...itkImage.spacing]
-      };
-
-      // Create VTK Image Data
-      const imageData = vtkImageData.newInstance(vtkImage);
-
-      // create VTK image data
-      const scalars = vtkDataArray.newInstance({
-        name: 'Scalars',
-        values: itkImage.data,
-        numberOfComponents: itkImage.imageType.components,
-      });
-
-      imageData.setDirection(...itkImage.direction.data);
-      imageData.setDimensions(...itkImage.size);
-      imageData.getPointData().setScalars(scalars);
-      
-      return Promise.resolve(imageData);
-    }
-
-    return Promise.reject("convertToVtkImage");
-    
+    return Promise.resolve(ITKHelper.convertItkToVtkImage(itkImage));
   }
   
 }
